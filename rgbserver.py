fc97635745d383ec31a7f539e23f8a7888940efb
Vishnu Vijayan <vvijayan@purdue.edu>
2017-09-09 00:07:13 -0400

    Initial commit

---
+++ rgbserver.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import termios
+import tty
+import pigpio
+import time
+
+# Pin numbers
+RED_PIN = 17
+GREEN_PIN = 22
+BLUE_PIN = 24
+
+pi = pigpio.pi()
+
+def updateColor(color, step):
+    color += step
+
+    if color > 255:
+        return 255
+    if color < 0:
+        return 0
+
+    return color
+
+def setLights(pin, brightness):
+	realBrightness = int(int(brightness) * (float(bright) / 255.0))
+	pi.set_PWM_dutycycle(pin, realBrightness)
+
+rgb(63,81,181)
+
+setLights(RED_PIN, 63)
+setLights(GREEN_PIN, 81)
+setLights(BLUE_PIN, 181)
+
+time.sleep(0.5)
+pi.stop()
