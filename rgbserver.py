f9653065ab86b918cf3b1d26670f1eb7d00906ce
Vishnu Vijayan <vvijayan@purdue.edu>
2017-12-19 01:06:38 +0000

    Fix server

--- rgbserver.py
+++ rgbserver.py
@@ -4,9 +4,17 @@ import termios
 import tty
 import pigpio
 import time
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+# Global Brightness
+bright = 255
+
+# Colors
+global red
+global green
+global blue
 
 # Pin numbers
 RED_PIN = 17
@@ -28,30 +36,59 @@ def updateColor(color, step):
 
 
 def setLights(pin, brightness):
+    brightness = int(brightness)
+
     if brightness < 0:
         brightness = 0
     if brightness > 255:
         brightness = 255
 
-        pi.set_PWM_dutycycle(pin, int(realBrightness))
+    realBrightness = int(int(brightness) * (float(bright) / 255.0))
+    
+    print(realBrightness)
+
+    pi.set_PWM_dutycycle(pin, realBrightness)
 
 
 @app.route('/')
 def homepage():
     return 'Pi RGB Server'
 
+@app.route('/currentColor')
+def currentColor():
+    col = {
+        'r': red,
+        'g': green,
+        'b': blue
+    }
+
+    return jsonify(col)
+
 
 @app.route('/set', methods=['POST'])
 def setColor():
-    color = request.get_json()
-    print(color)
-    setLights(RED_PIN, color['r'])
-    setLights(GREEN_PIN, color['g'])
-    setLights(BLUE_PIN, color['b'])
+    global red, green, blue
+
+    red = request.form['r']
+    green = request.form['g']
+    blue = request.form['b']
+
+    setLights(RED_PIN, red)
+    setLights(GREEN_PIN, green)
+    setLights(BLUE_PIN, blue)
 
     return 'true'
 
 if __name__ == '__main__':
+    red = 0
+    green = 30
+    blue = 30
+
+    setLights(RED_PIN, red)
+    setLights(GREEN_PIN, green)
+    setLights(BLUE_PIN, blue)
+
+    app.debug = True
     app.run(host='0.0.0.0', port=8008)
 
 # pi.stop()
